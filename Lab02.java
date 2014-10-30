83f0efec146470ca77c9a28311747102d6c9d55e
AlexanderFerragamo <alexander_ferragamo@asl.org>
2014-10-30 14:39:22 +0000

    Lab02

--- Lab02.java
+++ Lab02.java
@@ -2,16 +2,37 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Alexander Ferragamo>  // replace <...> with your name
+* @version <october 30> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
+ public class Athlete extends Robot
+   {
+   
+   public Athlete()
+   {
+   
+      super(1, 1, Display.NORTH, Display.INFINITY);
+      }
+      public Athlete(int x, int y, int dir, int beep);
+      {   
+      super(x, y, dir, beep);
+      }
+      public void turnAround();
+      {
+      turnLeft();
+      turnLeft();
+      }
+      public void turnRight();
+      {
+      turnLeft();
+      turnLeft();
+      turnLeft();
+      }
+   
 
-public class Lab02 {
-
-     public static void main(String[] args) {
-          // TODO Your code goes here
+   
      }
-}
+
